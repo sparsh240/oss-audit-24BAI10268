067fc9f066883a3c54c0038fe8a61e93d4e7d65a
sparsh <mailsparshgupta682@gmail.com>
2026-03-30 19:33:02 +0530

    Script 3

---
+++ script3.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+DIRS=("/etc" "/var/log" "/home" "/usr/bin" "/tmp")
+
+echo "Directory Audit Report"
+echo "----------------------"
+
+for DIR in "${DIRS[@]}"; do
+    if [ -d "$DIR" ]; then
+        PERMS=$(ls -ld $DIR | awk '{print $1, $3, $4}')
+        SIZE=$(du -sh $DIR 2>/dev/null | cut -f1)
+        echo "$DIR => Permissions: $PERMS | Size: $SIZE"
+    else
+        echo "$DIR does not exist"
+    fi
+done
+
+CONFIG_DIR="$HOME/.mozilla"
+
+if [ -d "$CONFIG_DIR" ]; then
+    ls -ld $CONFIG_DIR
+else
+    echo "Firefox config directory not found"
+fi

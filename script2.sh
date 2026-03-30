941c54999a1195e445215b81a04c1e280d751cb3
sparsh <mailsparshgupta682@gmail.com>
2026-03-30 19:32:54 +0530

    Script 2

---
+++ script2.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+PACKAGE="firefox"
+
+if dpkg -l | grep -qw $PACKAGE; then
+    echo "$PACKAGE is installed."
+    apt show $PACKAGE 2>/dev/null | grep -E 'Version|Maintainer|Description'
+else
+    echo "$PACKAGE is NOT installed."
+fi
+
+case $PACKAGE in
+    firefox) echo "Firefox: promotes privacy and open web" ;;
+    vlc) echo "VLC: open-source media player" ;;
+    git) echo "Git: version control system" ;;
+    apache2) echo "Apache: web server software" ;;
+    *) echo "Unknown package" ;;
+esac
